57f82b1798c1f12d6e3227bdc7fe6d452e2347da
Evsey Zholobov <evsei124@gmail.com>
2020-06-07 16:41:34 +0500

    description

--- GalaxyWebApi/GalaxyWebApi/Controllers/AuthController.cs
+++ GalaxyWebApi/GalaxyWebApi/Controllers/AuthController.cs
@@ -31,12 +31,13 @@ namespace GalaxyWebApi.Controllers
         /// <summary>
         /// Генерирует токен авторизации по указанному логину/паролю.
         /// </summary>       
-        /// <returns>Сгенерированный токен.</returns>        
+        /// <returns>Сгенерированный токен.</returns>  
+        /// <response code="200">Успешная авторизация</response>
+        /// <response code="401">Неверный логин/пароль</response>      
         [AllowAnonymous]
         [HttpPost]
-        [SwaggerResponse((int)HttpStatusCode.OK, Description = "Токен сгенерирован")]
-        [SwaggerResponse((int)HttpStatusCode.Unauthorized, Description = "Неверный логин/пароль")]
-        [SwaggerResponse((int)HttpStatusCode.InternalServerError, Description = "Внутренняя ошибка сервера")]
+        [SwaggerResponse((int)HttpStatusCode.OK)]
+        [SwaggerResponse((int)HttpStatusCode.Unauthorized)]
         public async Task<IActionResult> GetTokenAsync(AuthorizationDto authData)
         {
             var isAuthorized = await _authService.AuthorizeAsync(authData.Username, authData.Password);
@@ -48,11 +49,12 @@ namespace GalaxyWebApi.Controllers
             return Ok(token);
         }
 
+        /// <response code="200">Валидный токен</response>
+        /// <response code="400">Невалидный токен</response>
         [AllowAnonymous]
         [HttpGet]
-        [SwaggerResponse((int)HttpStatusCode.OK, Description = "Токен в валидном статусе")]
-        [SwaggerResponse((int)HttpStatusCode.BadRequest, Description = "Невалидный токен")]
-        [SwaggerResponse((int)HttpStatusCode.InternalServerError, Description = "Внутренняя ошибка сервера")]
+        [SwaggerResponse((int)HttpStatusCode.OK)]
+        [SwaggerResponse((int)HttpStatusCode.BadRequest)]
         public IActionResult ValidateToken(string token)
         {
             if (string.IsNullOrEmpty(token))
@@ -64,10 +66,11 @@ namespace GalaxyWebApi.Controllers
             return isValid ? (IActionResult)Ok() : BadRequest();
         }
 
+        /// <response code="200">Пароль успешно изменен</response>
+        /// <response code="400">Ошибка в теле запроса</response>
         [HttpPost]
-        [SwaggerResponse((int)HttpStatusCode.OK, Description = "Пароль успешно изменен")]
-        [SwaggerResponse((int)HttpStatusCode.BadRequest, Description = "Ошибка в теле запроса")]
-        [SwaggerResponse((int)HttpStatusCode.InternalServerError, Description = "Внутренняя ошибка сервера")]
+        [SwaggerResponse((int)HttpStatusCode.OK)]
+        [SwaggerResponse((int)HttpStatusCode.BadRequest)]
         public async Task<IActionResult> ChangePasswordAsync(ChangePasswordDto changePasswordData)
         {
             var isValidRequest = string.IsNullOrEmpty(changePasswordData.Username)
@@ -79,7 +82,7 @@ namespace GalaxyWebApi.Controllers
             var identityName = User.Identity.Name ?? string.Empty;
             var isIdentityNameNotEqualsRequested = !identityName.Equals(changePasswordData.Username, StringComparison.InvariantCultureIgnoreCase);
             if (isIdentityNameNotEqualsRequested)
-                return Unauthorized();
+                return BadRequest();
 
             var passwordsEquals = changePasswordData.NewPassword.Equals(changePasswordData.OldPassword, StringComparison.InvariantCultureIgnoreCase);
             if (passwordsEquals)
